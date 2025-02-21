d5dd6320858641cec0553fd931a639140a3c0ca4
adrianadermon <adrian.adermon@gmail.com>
2025-02-21 13:15:37 +0100

    Measurement error improvements

--- measurement-error/main.js
+++ measurement-error/main.js
@@ -32,8 +32,8 @@ const brd = JXG.JSXGraph.initBoard('jxgbox',
         showCopyright:false});
       
 // Sliders
-const errX = brd.create('slider', [[-8, -6], [0, -6], [0, 0, 2]], {name:'\\(\\lambda_x\\)'});
-errY = brd.create('slider', [[-8, -8], [0, -8], [0, 0, 2]], {name:'\\(\\lambda_y\\)'});
+const errX = brd.create('slider', [[-8, -6], [0, -6], [0, 0, 10]], {name:'\\(\\lambda_x\\)'}),
+errY = brd.create('slider', [[-8, -8], [0, -8], [0, 0, 10]], {name:'\\(\\lambda_y\\)'});
 
 
 
@@ -72,9 +72,9 @@ const add = JXG.Math.Statistics.add,
             sum = JXG.Math.Statistics.sum;
 
 // No. of points
-n = 30;
+n = 200;
 const x = [...Array(n).keys().map((x) => (20*x/(n-1) - 10))];
-const e = [...Array(n).keys().map((x) => JXG.Math.Statistics.randomNormal(0, 2))];
+const e = [...Array(n).keys().map((x) => JXG.Math.Statistics.randomNormal(0, 1))];
 const y = add(e,
     add(1,
         mult(0.5, x)
@@ -86,19 +86,13 @@ const ex = [...Array(n).keys().map((x) => JXG.Math.Statistics.randomNormal(0, 1)
 const ey = [...Array(n).keys().map((x) => JXG.Math.Statistics.randomNormal(0, 1))];
 
 
-// Plot true regression line
-let a, b;
-[a, b] = ols(x, y);
-
-const olsFit = function(x, y) {
-    return a + b*x;
-};       
-
-const regLine = brd.create('functiongraph', olsFit, {
-    strokeWidth: 3,
-    strokeColor: JXG.palette.blue
+// Plot data with errors
+x.forEach((x, i) => {
+    const y = 1 + 0.5*x + e[i];
+    brd.create('point', [() => {return x+errX.Value()*ex[i]},
+    () => {return y+errY.Value()*ey[i]}],
+    {name:'', size:2, color:JXG.palette.red, opacity: 0.5});
 });
-
 
 // Plot true data
 x.forEach((x, i) => {
@@ -108,28 +102,52 @@ x.forEach((x, i) => {
 });
 
 
-// Plot data with errors
-x.forEach((x, i) => {
-    const y = 1 + 0.5*x + e[i];
-    brd.create('point', [() => {return x+errX.Value()*ex[i]},
-    () => {return y+errY.Value()*ey[i]}],
-    {name:'', size:2, color:JXG.palette.red, opacity: 0.5});
-});
-
-
-
-let xObs = x+errX.Value()*ex[i]
-let yObs = y+errY.Value()*ey[i]
 
 // Plot regression line with errors
-let aObs, bObs;
-[aObs, bObs] = ols(xObs, yObs);
+const regLineObs = function(v) {
+    let xObs = add(x, mult(errX.Value(), ex));
+    let yObs = add(y, mult(errY.Value(), ey));
+    let aObs, bObs;
+    [aObs, bObs] = ols(xObs, yObs);
+    return aObs + bObs*v;
+};
 
-const regLineObs = brd.create('functiongraph',
-      () => {return aObs + bObs*xObs},
+const regLineObsPlot = brd.create('functiongraph',
+      regLineObs,
         {
             strokeWidth: 3,
-            strokeColor: JXG.palette.red
+            strokeColor: JXG.palette.red,
+            strokeOpacity: 0.5
         }
 );
 
+// Plot true regression line
+let a, b;
+[a, b] = ols(x, y);
+
+const olsFit = function(x, y) {
+    return 1 + 0.5*x;
+};       
+
+const regLine = brd.create('functiongraph', olsFit, {
+    strokeWidth: 3,
+    strokeColor: JXG.palette.blue
+});
+
+
+const regLineObsBeta = function() {
+    let xObs = add(x, mult(errX.Value(), ex));
+    let yObs = add(y, mult(errY.Value(), ey));
+    let aObs, bObs;
+    [aObs, bObs] = ols(xObs, yObs);
+    return bObs.toFixed(2);
+};
+
+// const MSEVal = function() {return 'MSE = ' + MSE().toFixed(2);};
+const trueBeta = '\\(\\beta\\) = ' + b.toFixed(2);
+const obsBeta = function() {return '\\(\\hat{\\beta}\\) = ' + regLineObsBeta();};
+
+brd.create('text', [5, -5, '\\(\\beta\\) = 0.5'], {fontSize: 18,
+    color:JXG.palette.blue});
+brd.create('text', [5, -7, obsBeta], {fontSize: 18,
+    color:JXG.palette.red});
