42a973874557657dbcc43c4d74d9cd88517fcc56
Adrian Adermon <adrian.adermon@gmail.com>
2025-03-28 14:19:02 +0100

    Fixed sigmoid function

--- modules/activation-functions.js
+++ modules/activation-functions.js
@@ -1,5 +1,5 @@
 function relu(x) { return Math.max(0, x) };
-function sigmoid(x) { return 1 / (1 + Math.E ** x) };
+function sigmoid(x) { return 1 / (1 + Math.E ** -x) };
 function leakyRelu(x) { return x >= 0 ? x : 0.1 * x };
 function hardSwish(x) {
         return (x < -3) ? 0 :
